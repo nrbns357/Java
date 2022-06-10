a3d90f10e06e6e6716d5b1a9055fb7eb8f96d1b7
Bae-Jin-Young <bae20051018@gmail.com>
2022-06-11 02:04:39 +0900

    FTP Server

--- network/FTP/client/ClientMain.java
+++ network/FTP/client/ClientMain.java
@@ -1,24 +1,22 @@
 package kr.hs.dgsw.network.test01.n2212.client;
 
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.List;
+
 public class ClientMain {
 
     private static Scanner scanner;
     private static Socket sk;
 
     static OutputStream os = null;
-   static DataOutputStream dos = null;
+    static DataOutputStream dos = null;
 
-   static InputStream is = null;
+    static InputStream is = null;
     static DataInputStream dis = null;
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
 
         try {
@@ -33,31 +31,28 @@ public class ClientMain {
             is = sk.getInputStream();
             dis = new DataInputStream(is);
 
-            Boolean isLogin;
+            clientLogin();
+
+
             while (true) {
-               isLogin = dis.readBoolean();
-               if(!isLogin) {
-                   System.out.printf("ID:");
-                   String id = scanner.nextLine();
-                   System.out.printf("Pass:");
-                   String pw = scanner.nextLine();
-//                   String idpw = id + pw;
-                   dos.writeUTF(id);
-                   dos.writeUTF(pw);
-                   String slm = dis.readUTF();
-                   System.out.println(slm);
-               }
-               else{
-                  break;
-               }
-            }
-            while (true){
                 String command = scanner.nextLine();
-                String Ccomand = command.substring(0,command.indexOf(" "));
-//                       System.out.println(Ccomand);
-                switch (Ccomand) {
+                String[] onlyCommand = command.split(" ");
+
+                switch (onlyCommand[0].replace("\n","")) {
                     case ("/업로드"):
-                        uploadCommand(command);
+                        uploadCommand(command, onlyCommand[0]);
+                        break;
+                    case ("/다운로드"):
+                        doDownload(command, onlyCommand[0]);
+                        break;
+                    case ("/파일목록"):
+                        showFileList(onlyCommand[0]);
+                        break;
+                    case ("/exit"):
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("존재하지 않는 명령어 입니다");
                 }
             }
         } catch (Exception e) {
@@ -65,24 +60,113 @@ public class ClientMain {
         }
     }
 
-    public static void uploadCommand(String command) throws IOException {
-//        System.out.println(command);
+    private static void clientLogin() throws IOException {
+        while (true) {
+            String isLoginInfo = dis.readUTF();
+            String[] isLoginInfoList = isLoginInfo.split(" ");
+            if (isLoginInfoList[1].equals("false")) {
+                System.out.printf("ID:");
+                String id = scanner.nextLine();
+                if(id.length() == 0) id = "?";
+                System.out.printf("Pass:");
+                String pw = scanner.nextLine();
+                if(pw.length() == 0) pw = "?";
+                dos.writeUTF("[Type]로그인: " + id + " " + pw);
+                System.out.println(dis.readUTF());
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void uploadCommand(String command, String onlyCommand) throws IOException {
+        dos.writeUTF("[Type]명령어구분: " + onlyCommand);
         String filepath = command.substring(5, command.lastIndexOf("/"));
-        String filenameList = command.substring(command.lastIndexOf("/")+1);
-        System.out.println("경로"+filepath);
-        System.out.println("파일"+filenameList);
-//        String fullfilePath = filepath+"/"+filenameList;
-        File fl = new File(filepath+"/"+filenameList);
-        FileInputStream fis = new FileInputStream(fl);
+        String filenameList = command.substring(command.lastIndexOf("/") + 1);
+        String[] fileList = filenameList.split(" ");
 
-        int readsize = 0;
-        byte[] bytes = new byte[1024];
+        File fl = new File(filepath + "/" + fileList[0]);
+        if (fl.exists()) {
+            FileInputStream fis = new FileInputStream(fl);
 
+            int readsize = 0;
+            byte[] bytes = new byte[1024];
+            if (fileList.length == 2) {
+                dos.writeUTF("[Type]업로드: " + fileList[1]);
+            } else {
+                dos.writeUTF("[Type]업로드: " + fileList[0]);
+            }
+            String same = dis.readUTF();
+            String[] sameInfo = same.split(" ");
+            if (sameInfo[1].equals("true")) {
+                System.out.print("파일이 이미 있습니다. 덮어쓰기 하실건가요??(Yes: 덮어쓰기 / No: 업로드 취소):");
+                String isChange = scanner.nextLine();
+                dos.writeUTF("[Type]파일교체여부: " + isChange);
 
-        dos.writeUTF(fl.getName());
+                String cancel = dis.readUTF();
+                String[] cancelInfo = cancel.split(" ");
+                if (cancelInfo[1].equals("취소")) {
+                    System.out.println("업로드가 취소되었습니다");
+                }else{
+                    while ((readsize = fis.read(bytes)) != -1) {
+                        dos.write(bytes, 0, readsize);
+                        if (readsize != 1024) {
+                            break;
+                        }
+                    }
+                    System.out.println("업로드가 완료되었습니다");
+                }
+            }else{
+                while ((readsize = fis.read(bytes)) != -1) {
+                    dos.write(bytes, 0, readsize);
+                    if (readsize != 1024) {
+                        break;
+                    }
+                }
+                System.out.println("업로드가 완료되었습니다");
+            }
 
-        while((readsize=fis.read(bytes))!=-1) {
-            dos.write(bytes,0,readsize);
+        } else {
+            System.out.println("파일이 존재하지 않습니다");
+        }
+
+    }
+
+    public static void showFileList(String onlyCommand) throws IOException {
+        dos.writeUTF("[Type]명령어구분: " + onlyCommand);
+        String fileNum = dis.readUTF();
+        String[] FileListReqInfo = fileNum.split(" ");
+        for (int i = 0; i < Integer.parseInt(FileListReqInfo[1]); i++) {
+            String FileList = dis.readUTF();
+            String[] FileListInfo = FileList.split(" ");
+            System.out.println(FileListInfo[1] + " " + FileListInfo[2]);
+        }
+    }
+
+    public static void doDownload(String command, String onlyCommand) throws IOException {
+        String Download = "D:/download/";
+        String filename = command.substring(6);
+        dos.writeUTF("[Type]명령어구분: " + onlyCommand);
+        dos.writeUTF("[Type]다운로드: "+filename);
+        String DownFileInfo = dis.readUTF();
+        String[] DownFile = DownFileInfo.split(" ");
+
+        FileOutputStream fos = new FileOutputStream(Download + DownFile[2]);
+
+        if (DownFile[1].equals("true")) {
+            int readsize = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((readsize = dis.read(bytes)) != -1) {
+                fos.write(bytes, 0, readsize);
+                if (readsize != 1024) {
+                    break;
+                }
+
+            }
+            System.out.println("**" + DownFile[2] + "을 " + Download + "로 다운로드 하였습니다 **");
+        } else {
+            System.out.println(DownFile[2] + "파일이 존재하지 않습니다");
         }
     }
 }
--- network/FTP/server/ServerMain.java
+++ network/FTP/server/ServerMain.java
@@ -3,65 +3,184 @@ package kr.hs.dgsw.network.test01.n2212.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Locale;
 
-public class ServerMain extends Thread{
+public class ServerMain extends Thread {
 
+    static String id = "admin";
+    static String pw = "1234";
+    static String filefolder = "D:/networkTest01";
+    private OutputStream os = null;
+    private DataOutputStream dos = null;
+
+    private InputStream is = null;
+    private DataInputStream dis = null;
+    static ServerSocket ss;
+    private Socket sk;
+
+    private int readsize = 0;
+    private byte[] bytes = new byte[1024];
+
+    private String isLogin = "false";
 
     public static void main(String[] args) {
-//        String cidpw = "admin1234";
-        String id = "admin";
-        String pw = "1234";
-        String filefolder = "D:/networkTest01";
-        OutputStream os = null;
-        DataOutputStream dos = null;
-
-        InputStream is = null;
-        DataInputStream dis = null;
-
-        boolean isLogin = false;
-
         try {
-            ServerSocket ss = new ServerSocket(5000);
-            Socket sk = ss.accept();
+            ss = new ServerSocket(5000);
+            while(true) {
+                new ServerMain(ss.accept());
+            }
+        } catch (Exception e) {
+        }
+    }
 
+    public ServerMain(Socket socket) {
+        this.sk = socket;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        try {
             os = sk.getOutputStream();
             dos = new DataOutputStream(os);
 
             is = sk.getInputStream();
             dis = new DataInputStream(is);
+            Login();
+
             while (true) {
+                String uploadInfo = dis.readUTF();
+                String[] uploadInfoList = uploadInfo.split(" ");
+                String command = uploadInfoList[1];
+                switch (command) {
+                    case ("/업로드"):
+                        upload();
+                        break;
 
-                dos.writeBoolean(isLogin);
+                    case ("/파일목록"):
+                        doShowFileList();
+                        break;
 
-                String clmi = dis.readUTF();
-                String clmp = dis.readUTF();
-//                System.out.println(clm);
-
-                if (clmi.equals(id) &&clmp.equals(pw) ) {
-                    dos.writeUTF("** FTP 서버에 접속하였습니다. **");
-                    isLogin = true;
-                    dos.writeBoolean(isLogin);
-                    break;
-
-                } else {
-                    dos.writeUTF("** ID 또는 PASS가 틀렸습니다.! **");
-                }
-            }
-            while (true){
-                String filename = dis.readUTF();
-                System.out.println(filename);
-                FileOutputStream fos = new FileOutputStream(filefolder+"/"+ filename);
-                int readsize = 0;
-                byte[] bytes = new byte[1024];
-
-                while ((readsize = dis.read(bytes)) != -1) {
-                    fos.write(bytes, 0, readsize);
+                    case ("/다운로드"):
+//
+                        doDownLoad();
+                        break;
                 }
             }
         } catch (Exception e) {
+        } finally {
+            System.out.println(sk.getInetAddress() + "님이 나갔습니다.");
+        }
+    }
+
+    private void doDownLoad() throws IOException {
+        String[] serverFileName = dis.readUTF().split(" ");
+
+        File fl = new File(filefolder + "/" + serverFileName[1]);
+        if (fl.exists()) {
+            FileInputStream fis = new FileInputStream(fl);
+            dos.writeUTF("[Type]다운로드파일정보: true " + fl.getName());
+            readsize = 0;
+            bytes = new byte[1024];
+
+            while ((readsize = fis.read(bytes)) != -1) {
+                dos.write(bytes, 0, readsize);
+                if (readsize != 1024) {
+                    break;
+                }
+            }
+        } else {
+            dos.writeUTF("[Type]다운로드파일정보: false " + fl.getName());
+        }
+    }
+
+    private void doShowFileList() throws IOException {
+        System.out.println("파일목록");
+        File dir = new File(filefolder);
+        File[] childrenFiles = dir.listFiles();
+        dos.writeUTF("[Type]파일목록개수: " + childrenFiles.length);
+        for (File file : childrenFiles) {
+            dos.writeUTF("[Type}파일리스트: **" + file.getName() + " " + lengthGMK(file.length()) + "**");
+        }
+    }
+
+    private void upload() throws IOException {
+        String filenameReq = dis.readUTF();
+        String[] filename = filenameReq.split(" ");
+
+        File fl = new File(filefolder + "/" + filename[1]);
+        if (fl.exists()) {
+            FileOutputStream fos = new FileOutputStream(filefolder + "/" + filename[1]);
+            dos.writeUTF("[Type]중복: true");
+            String changeFile = dis.readUTF();
+            String[] changeFileInfo = changeFile.split(" ");
+            if (changeFileInfo[1].toLowerCase(Locale.ROOT).equals("yes")) {
+                dos.writeUTF("[Type]업로드: 확인");
+                while (true) {
+                    readsize = dis.read(bytes);
+                    fos.write(bytes, 0, readsize);
+                    if (readsize != 1024) {
+                        break;
+                    }
+                }
+            } else {
+                dos.writeUTF("[Type]업로드: 취소");
+            }
+        } else {
+            dos.writeUTF("[Type]중복: false");
+            FileOutputStream fos = new FileOutputStream(filefolder + "/" + filename[1]);
+            while (true) {
+                readsize = dis.read(bytes);
+                fos.write(bytes, 0, readsize);
+                if (readsize != 1024) {
+                    break;
+                }
+            }
+        }
+    }
+
+    private void Login() throws IOException {
+
+        while (true) {
+            dos.writeUTF("[Type]로그인: " + isLogin);
+
+            String loginInfo = dis.readUTF();
+            String[] loginInfoList = loginInfo.split(" ");
+            String rid = loginInfoList[1];
+            String rpw = loginInfoList[2];
+            if (id.equals(rid) && pw.equals(rpw)) {
+                dos.writeUTF("** FTP 서버에 접속하였습니다. **");
+                isLogin = "true";
+                dos.writeUTF("[Type]로그인: " + isLogin);
+                break;
+
+            } else {
+                dos.writeUTF("** ID 또는 PASS가 틀렸습니다.! **");
+            }
 
         }
 
+    }
 
+
+    public String lengthGMK(double size) {
+        String GMK = "";
+
+        if (size < 1000) {
+            GMK = size + "";
+        } else if (size >= Math.pow(2, 30)) {
+
+            GMK = roundGMK(size, 30) + "G";
+        } else if (size >= Math.pow(2, 20)) {
+            GMK = roundGMK(size, 20) + "M";
+        } else if (size >= Math.pow(2, 10)) {
+            GMK = roundGMK(size, 10) + "kb";
+        }
+
+        return GMK;
+    }
+
+    private double roundGMK(double size, int x) {
+        return Math.round(size / Math.pow(2, x) * 100) / 100.0;
     }
 }
